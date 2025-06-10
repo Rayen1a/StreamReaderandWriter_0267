2b2d1e21b46cef86d44c8b575a36dfc287d557d3
Rayen1a <raihan01426@gmail.com>
2025-06-10 10:48:47 +0700

    membuat int main stringbaris,ofstream outfile dan membuka lalu menulis file

--- iosFileWithpath.cpp
+++ iosFileWithpath.cpp
@@ -2,3 +2,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "masukan nama File : ";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+}
